1b86f66490dd7b7acb65d012ad09d4fbd4ceef7b
brianchanoocl <Delubdi118>
2021-12-03 13:57:14 +0800

    [ParkingLot] [Brian] - feat: init project

---
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -0,0 +1,11 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// [ParkingLot] [Brian] - feat: 
+
+public class ParkingLotTest {
+
+}
