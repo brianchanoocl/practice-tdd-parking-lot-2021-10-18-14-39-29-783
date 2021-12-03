6b136ca69fff7795993ef967cdd51d68807e8fa5
brianchanoocl <Delubdi118>
2021-12-03 15:53:03 +0800

    [ParkingLot] [Brian] - feat: Implemented code passed should_throw_unrecognized_parking_ticket_exception_when_fetch_car_given_parking_lot_and_ticket_is_used()

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -82,20 +82,6 @@ public class ParkingLotTest {
     }
 
     @Test
-    void should_return_null_when_fetch_car_given_parking_lot_and_ticket_is_used() {
-        ParkingLot parkingLot = new ParkingLot();
-        Car car = new Car();
-        Ticket ticket = parkingLot.parkCar(car);
-        parkingLot.parkCar(car);
-
-        parkingLot.fetchCar(ticket);
-        Car fetchedCar = parkingLot.fetchCar(ticket);
-
-        assertNull(fetchedCar);
-    }
-
-//////////////////////
-    @Test
     void should_throw_unrecognized_parking_ticket_exception_when_fetch_car_given_parking_lot_and_ticket_is_used() {
         ParkingLot parkingLot = new ParkingLot();
         Car car = new Car();
