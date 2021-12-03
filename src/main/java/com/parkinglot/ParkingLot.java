3fd6a060ab4df17b357385da1c02eff4b16f3aeb
brianchanoocl <Delubdi118>
2021-12-03 14:11:57 +0800

    [ParkingLot] [Brian] - feat: complete code implementation for test case should_return_null_when_park_car_given_parking_lot_is_full_and_car()

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,17 +1,26 @@
 package com.parkinglot;
 
+import java.util.ArrayList;
+
 public class ParkingLot {
     private final int capacity;
+    private ArrayList<Car> carList;
 
     public ParkingLot() {
         this.capacity = 10;
+        carList = new ArrayList<Car>();
     }
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
+        carList = new ArrayList<Car>();
     }
 
     public Ticket parkCar(Car car) {
+        if(carList.size()+1 > capacity){
+            return null;
+        }
+        carList.add(car);
         return new Ticket(car);
     }
 }
