ce631d98168fc70f228abc132cdfd84871c24bd3
brianchanoocl <Delubdi118>
2021-12-03 14:27:43 +0800

    [ParkingLot] [Brian] - feat: Implemented code passed should_return_2_correct_car_when_fetch_2_cars_given_parking_lot_and_2_tickets()

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// [ParkingLot] [Brian] - feat:
+// [ParkingLot] [Brian] - feat: 
 
 public class ParkingLotTest {
     @Test
