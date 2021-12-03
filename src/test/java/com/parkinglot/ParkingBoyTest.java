2246955b73ad63c0b82a0c634b492496a72ce112
brianchanoocl <Delubdi118>
2021-12-03 16:37:49 +0800

    [ParkingLot] [Brian] - feat: Create ParkingBoy class, add and complete Stroy 3 AC1

---
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -0,0 +1,18 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParkingBoyTest {
+    @Test
+    void should_return_ticket_when_park_car_given_parking_boy_with_1_parking_lot_and_car() {
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+
+        Ticket ticket = parkingBoy.parkCar(new Car());
+
+        assertNotNull(ticket);
+    }
+
+}
