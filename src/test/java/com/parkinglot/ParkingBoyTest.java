4f634306d2b5a3a86dffed62ef1669765bc45792
brianchanoocl <Delubdi118>
2021-12-03 17:08:05 +0800

    [ParkingLot] [Brian] - feat: Created test case should_return_ticket_for_parking_lot_2_when_park_car_given_parking_boy_with_2_parking_lot_and_parking_lot_1_fulled_and_car()

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -18,7 +18,7 @@ public class ParkingBoyTest {
     }
 
     @Test
-    void should_return_ticket_when_park_car_given_parking_boy_with_2_parking_lot_and_car() {
+    void should_return_ticket_for_parking_lot_1_when_park_car_given_parking_boy_with_2_parking_lot_and_car() {
         ParkingLot parkingLot1 = new ParkingLot();
         ParkingLot parkingLot2 = new ParkingLot();
         ArrayList<ParkingLot> parkingLots = new ArrayList<ParkingLot>();
@@ -31,5 +31,22 @@ public class ParkingBoyTest {
         assertNotNull(ticket);
         assertEquals(9, parkingLot1.getAvailablePosition());
         assertEquals(10, parkingLot2.getAvailablePosition());
+    }
+
+    @Test
+    void should_return_ticket_for_parking_lot_2_when_park_car_given_parking_boy_with_2_parking_lot_and_parking_lot_1_fulled_and_car() {
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot();
+        ArrayList<ParkingLot> parkingLots = new ArrayList<ParkingLot>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+
+        parkingBoy.parkCar(new Car());
+        Ticket ticket = parkingBoy.parkCar(new Car());
+
+        assertNotNull(ticket);
+        assertEquals(0, parkingLot1.getAvailablePosition());
+        assertEquals(9, parkingLot2.getAvailablePosition());
     }
 }
