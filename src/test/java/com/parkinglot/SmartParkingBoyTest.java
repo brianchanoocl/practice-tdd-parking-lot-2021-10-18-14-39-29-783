cf72ea14ec084706114136931442c674152c9011
brianchanoocl <Delubdi118>
2021-12-04 20:57:45 +0800

    [ParkingLot] [Brian] - feat: Created test case should_return_ticket_in_second_parking_lot_when_park_car_given_smart_parking_boy_with_2_parking_lots_and_secopnd_parking_lot_has_more_empty_position_and_car()

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -23,4 +23,20 @@ public class SmartParkingBoyTest {
         assertEquals(9, parkingLot1.getAvailablePosition());
         assertEquals(10, parkingLot2.getAvailablePosition());
     }
+
+    @Test
+    void should_return_ticket_in_second_parking_lot_when_park_car_given_smart_parking_boy_with_2_parking_lots_and_secopnd_parking_lot_has_more_empty_position_and_car() {
+        ParkingLot parkingLot1 = new ParkingLot(5);
+        ParkingLot parkingLot2 = new ParkingLot();
+        ArrayList<ParkingLot> parkingLots = new ArrayList<ParkingLot>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+
+        Ticket ticket = smartParkingBoy.parkCar(new Car());
+
+        assertNotNull(ticket);
+        assertEquals(5, parkingLot1.getAvailablePosition());
+        assertEquals(9, parkingLot2.getAvailablePosition());
+    }
 }
